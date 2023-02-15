850ccc9947eacef14b760bbeb3586749804451d8
Aaron Tandem <aaronlbiever@gmail.com>
2023-02-15 11:44:25 +0900

    Updated some CSS and text, and finalized basic download button functionality

--- src/components/Help.js
+++ src/components/Help.js
@@ -20,22 +20,40 @@ export default function HelpModal(props) {
             <h2>about</h2>
             <p>This delightfully simple site is designed to be your personal birdhouse — an avian escape where you can relax and just focus on expressing your tweets and warbles.</p>
             <br></br>
-            <p>settle down in your roost and incubate your thoughts before you send them off to fly all on their own.</p>
-            <br></br>
-            <p>you can't actually tweet from here — yet — it's just a place to write them. Think of it as a journal where you hatch your initial drafts.</p>
+            <p>you can't actually tweet from here — yet — it's just a place to incubate your thoughts. Think of it as a nest where you hatch your initial drafts before sending them off to fly all on their own.</p>
             <br></br>
             <p>if you have any feedback, need a bug to be pecked, or simply have a suggestion for a new feature, please don't hesitate to send a raven to:</p>
             <br></br>
             <p><a href='mailto:aaronlbtandem@gmail.com'>aaronlbtandem@gmail.com</a></p>
             <h2 className='convenient-keystrokes'>keystrokes</h2>
-            <p><b>add textbox</b> — shift + enter</p>
-            <p><b>delete textbox</b> — shift + tab</p>
-            <p><b>save & reset</b> — left click button</p>
-            <h2>saved tweets</h2>
-            <p>when you save your tweets and the writing area resets, your tweets will be saved here.</p>
+            <p><b>add (+) textbox</b> — shift + enter</p>
+            <p><b>delete (-) textbox</b> — shift + tab</p>
+            <p><b>save & reset</b> — left click middle button</p>
+            <h2>saved chirps</h2>
+            <p>after clicking save and the writing area resets, your chirps will be saved here available for download.</p>
             <br></br>
-            <p><em>{props.savedContent.length ? `You have saved ${props.savedContent.length} tweets.` : 'You have not saved any tweets yet.'}</em></p>
-            <button onClick={handleDownload}>Download</button>
+            <p>
+              <em>
+              {
+                props.savedContent.length ?
+              
+                <span id='download-span'>
+                   
+                  <button 
+                    onClick={handleDownload}
+                    className="download-button"
+                    >
+                    <span className="material-symbols-outlined download-icon">
+                    download
+                    </span>
+                  </button>
+                  download {props.savedContent.length} chirps 
+                </span>
+            
+                : 'you have not saved any chirps yet.'
+              }
+              </em>
+            </p>
         </div>
       )}
     </div>
